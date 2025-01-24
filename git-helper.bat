31aa0f5e6cb5a597a90b435b23aca59de96da772
Ebubechukwu Ugbor <ugbore2@gmail.com>
2025-01-24 20:40:37 +0100

    working

--- git-helper.bat
+++ git-helper.bat
@@ -3,16 +3,20 @@
 REM Check if a commit message is provided
 IF "%~1"=="" (
     echo Error: No commit message provided.
-    echo Usage: git-helper "Your commit message"
+    echo Usage: git-helper "Your commit message" [branch-name]
     exit /b 1
 )
+
+REM Set the branch name
+SET BRANCH_NAME=%2
+IF "%BRANCH_NAME%"=="" SET BRANCH_NAME=master
 
 REM Run Git commands
 git add .
 git commit -m "%~1"
-git push
+git push origin %BRANCH_NAME%
 
-FOR /F "tokens=*" %%g IN ('git push 2^>^&1') DO (
+FOR /F "tokens=*" %%g IN ('git push origin %BRANCH_NAME% 2^>^&1') DO (
     IF NOT "%%g"=="Everything up-to-date" (
         echo %%g
     )
